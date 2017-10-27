5b4a7e010b6da20fdd06c313458da8e76b6bc67b
Zachary Becker <zrbecker@gmail.com>
2017-10-27 01:45:19 -0700

    Add environment variable to force HTTPS and hostname.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import path from 'path'
 import process from 'process'
+import sslify from 'express-sslify'
+import forceDomain from 'express-force-domain';
 
 const app = express()
 app.set('port', process.env.PORT || 8080)
 
+if (process.env.FORCE_DOMAIN) {
+  app.use(forceDomain(process.env.FORCE_DOMAIN))
+}
+if (process.env.FORCE_HTTPS) {
+  app.use(sslify.HTTPS({ trustProtoHeader: true }))
+}
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(app.get('port'), () => console.log('express is listening...'))
