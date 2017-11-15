983b3b6dfade750072f6989c0484a61f9b7ce405
Zachary Becker <zrbecker@gmail.com>
2017-11-14 16:40:50 -0800

    added home page

--- client-src/app/home/home-view.jsx
+++ client-src/app/home/home-view.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
+import FontAwesome from 'react-fontawesome'
 
 import photo256 from '../images/website_photo_256.jpg'
 
 const HomeView = () => {
   return (
-    <div class="container">
+    <div className="container" style={{textAlign: 'center'}}>
       <header>
         <h1>Zachary Becker</h1>
         <img src={photo256} />
       </header>
       <section>
-        <p>Email: <a href="mailto:zrbecker@gmail.com">zrbecker@gmail.com</a></p>
-        <p>Linkedin: URL</p>
-        <p>Github: URL</p>
-        <p>Resume: URL</p>
+        <p>
+          I am a software engineer with interest in data science and machine
+          learning. Additionally I've been learning about full stack javascript
+          development, web security, and cloud deployments.
+        </p>
+      </section>
+      <section>
+        <p>
+          <span style={{padding: '0 0.5em'}}>
+            <FontAwesome name='envelope' />
+            <a href='mailto:zrbecker@gmail.com'>Email</a>
+          </span>
+          <span style={{padding: '0 0.5em'}}>
+            <FontAwesome name='linkedin' />{' '}
+            <a href='#'>LinkedIn</a>
+          </span>
+          <span style={{padding: '0 0.5em'}}>
+            <FontAwesome name='github' />{' '}
+            <a href='#'>Github</a>
+          </span>
+        </p>
       </section>
     </div>
   )
